9278559b0b61b9a00493e131c3371254d9295762
dinafuku <dinafuku@fowler-lpw-dgx-0.chapman.edu>
2024-03-11 22:20:07 -0700

    added eda and simple rf_model

---
+++ eda.py
@@ -0,0 +1,71 @@
+import os
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# paths to data
+images_folder = 'data/images'
+masks_folder = 'data/masks'
+output_folder = 'eda_visuals'
+
+# list of images/masks
+image_files = sorted(os.listdir(images_folder))
+mask_files = sorted(os.listdir(masks_folder))
+
+# print number of images/masks
+num_images = len(image_files)
+num_masks = len(mask_files)
+
+print(f"Number of images: {num_images}")
+print(f"Number of masks: {num_masks}\n")
+
+# load images/masks
+def load_images_and_masks(image_files, mask_files):
+    images = []
+    masks = []
+    for img_file, mask_file in zip(image_files, mask_files):
+        img_path = os.path.join(images_folder, img_file)
+        mask_path = os.path.join(masks_folder, mask_file)
+        image = Image.open(img_path)
+        mask = Image.open(mask_path)  
+        images.append(image)
+        masks.append(mask)
+    return images, masks
+
+# load images and masks
+images, masks = load_images_and_masks(image_files, mask_files)
+
+# plot each pair of image and mask in one figure
+num_examples = 10
+for i in range(num_examples):
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    axes[0].imshow(images[i])
+    axes[0].set_title('Image')
+    axes[0].axis('off')
+    
+    axes[1].imshow(masks[i])
+    axes[1].set_title('Mask')
+    axes[1].axis('off')
+    
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_folder, f'image_mask_{i+1}.png'))
+    plt.close()
+
+# get unique dimensions of images to see difference in image sizes
+def get_unique_dimensions(image_files, mask_files):
+    dimensions = set()
+    for img_file, mask_file in zip(image_files, mask_files):
+        img_path = os.path.join(images_folder, img_file)
+        mask_path = os.path.join(masks_folder, mask_file)
+        image_size = Image.open(img_path).size
+        mask_size = Image.open(mask_path).size
+        dimensions.add(image_size)
+        dimensions.add(mask_size)
+    return dimensions
+
+# get unique dimensions
+unique_dimensions = get_unique_dimensions(image_files, mask_files)
+
+# print unique dimensions
+print("Unique dimensions of images and masks:")
+for dim in unique_dimensions:
+    print(dim)
---
+++ rf_model.py
@@ -0,0 +1,71 @@
+# import libraries
+import os
+import time
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, jaccard_score
+
+# path to data
+images_folder = 'data/images'
+masks_folder = 'data/masks'
+
+# get the list of image and mask files
+image_files = sorted(os.listdir(images_folder))[:75]
+mask_files = sorted(os.listdir(masks_folder))[:75]
+
+# load, preprocess images/masks
+def load_and_preprocess_data(image_files, mask_files, img_size=(224, 224)):
+    images = []
+    masks = []
+    for img_file, mask_file in zip(image_files, mask_files):
+        img_path = os.path.join(images_folder, img_file)
+        mask_path = os.path.join(masks_folder, mask_file)
+        image = Image.open(img_path).resize(img_size)  # resize image
+        mask = Image.open(mask_path).resize(img_size).convert('L')  # resize and convert to grayscale
+        images.append(np.array(image))
+        masks.append(np.array(mask))
+    return np.array(images), np.array(masks)
+
+load_start = time.time()
+# load and preprocess images and masks
+images, masks = load_and_preprocess_data(image_files, mask_files)
+load_end = time.time()
+print(f"Preprocessing took {load_end-load_start} seconds...")
+
+# flatten images and masks for RF
+X_train_flat = images.reshape(len(images), -1)  
+y_train_flat = masks.reshape(len(masks), -1)    
+
+# TTS (80/20)
+X_train, X_test, y_train, y_test = train_test_split(X_train_flat, y_train_flat, test_size=0.2, random_state=542)
+
+# create/fit RF model
+rf_model = RandomForestClassifier(n_estimators=1, random_state=542)
+fit_start = time.time()
+rf_model.fit(X_train, y_train)
+fit_end = time.time()
+print(f"Fitting took {fit_end-fit_start} seconds...")
+
+# predict masks for train and test data
+train_pred_masks = rf_model.predict(X_train)
+test_pred_masks = rf_model.predict(X_test)
+
+# calculate train acc
+train_accuracy = accuracy_score(y_train.flatten(), train_pred_masks.flatten())
+
+# calculate IOU score for training data using jaccard_score
+train_iou = jaccard_score(y_train.flatten(), train_pred_masks.flatten(), average='macro')
+
+# calculate test acc
+test_accuracy = accuracy_score(y_test.flatten(), test_pred_masks.flatten())
+
+# calculate IOU score for testing data using jaccard_score
+test_iou = jaccard_score(y_test.flatten(), test_pred_masks.flatten(), average='macro')
+
+# print out results
+print("\nTraining Accuracy: ", train_accuracy)
+print("Testing Accuracy: ", test_accuracy)
+print("Training IoU: ", train_iou)
+print("Testing IoU: ", test_iou)
